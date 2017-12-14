19736a4b82a8e3f17396fdcd5d6e447de78deb89
awmleer <awmleer@sparker.xyz>
2017-12-14 16:36:57 +0800

    support input directory

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -5,40 +5,41 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv ) {
-    char image_name[20];
+    char imagePath[200];
 
     //initialize video writer
-    cv::VideoWriter writer;
+    VideoWriter writer;
     bool isColor = true;
-    int frame_fps = 30;
-    int frame_width = 640;
-    int frame_height = 360;
-    Size finalSize = Size(frame_width,frame_height);
-    string video_name = "out.avi";
-    writer = VideoWriter(video_name, CV_FOURCC('M', 'J', 'P', 'G'), frame_fps, finalSize,isColor);
+    int frameFps = 30;
+    int frameWidth = 640;
+    int frameHeight = 360;
+    Size finalSize = Size(frameWidth,frameHeight);
+    string videoName = "out.avi";
+    writer = VideoWriter(videoName, CV_FOURCC('M', 'J', 'P', 'G'), frameFps, finalSize,isColor);
 
     //join images
-    int imageAmount = 2;//amount of images
     int i = 0;
     Mat img;
-    while (i<imageAmount) {
-        sprintf(image_name, "%s%d%s", "image", ++i, ".png");
-        cout << image_name <<endl;
-        img = imread(image_name);//read images
-
+    while (true) {
+        sprintf(imagePath, "%s/%d.jpg", argv[1], ++i);
+        img = imread(imagePath);//read images
         if (!img.data) {
-            throw "Error when reading image file";
+            break;
         }
+        cout << imagePath <<endl;
         //写入
         Mat imgResized;
         resize(img, imgResized, finalSize);
+        putText(imgResized, "3150104785 HaoGuangBo", cvPoint(30,320), FONT_HERSHEY_COMPLEX_SMALL, 1.0, cvScalar(200,200,250), 1, CV_AA);
         for(int j=0; j<60; j++){// 60 frames = 2 seconds
             writer.write(imgResized);
         }
     }
 
     //handle video
-    VideoCapture capture = VideoCapture("1.avi");
+    char videoPath[200];
+    sprintf(videoPath, "%s/1.avi", argv[1]);
+    VideoCapture capture = VideoCapture(videoPath);
     Mat frame,frameResized;
     cout<<"1.avi"<<endl;
     if( !capture.isOpened() ){
