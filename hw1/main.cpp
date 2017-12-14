3001f5c33961316fcfc813946f601044aebba341
awmleer <awmleer@sparker.xyz>
2017-12-14 16:10:54 +0800

    read video and add text on it

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <string>
 
 using namespace cv;
 using namespace std;
@@ -8,37 +7,54 @@ using namespace std;
 int main(int argc, char** argv ) {
     char image_name[20];
     string s_image_name;
+
+    //initialize video writer
     cv::VideoWriter writer;
     int isColor = 1;
     int frame_fps = 30;
     int frame_width = 640;
-    int frame_height = 480;
-    Size size = Size(frame_width,frame_height);
+    int frame_height = 360;
+    Size finalSize = Size(frame_width,frame_height);
     string video_name = "out.avi";
-    writer = VideoWriter(video_name, CV_FOURCC('M', 'J', 'P', 'G'), frame_fps, size,isColor);
-    cout << "frame_width is " << frame_width << endl;
-    cout << "frame_height is " << frame_height << endl;
-    cout << "frame_fps is " << frame_fps << endl;
-    int num = 2;//输入的图片总张数
+    writer = VideoWriter(video_name, CV_FOURCC('M', 'J', 'P', 'G'), frame_fps, finalSize,isColor);
+
+    //join images
+    int imageAmount = 2;//amount of images
     int i = 0;
     Mat img;
-    while (i<num) {
+    while (i<imageAmount) {
         sprintf(image_name, "%s%d%s", "image", ++i, ".png");
         cout << image_name <<endl;
         s_image_name = image_name;
-        img = imread(s_image_name);//读入图片
+        img = imread(s_image_name);//read images
 
         if (!img.data) {
-            cout << "Could not load image file...\n" << endl;
+            throw "Error when reading image file";
         }
         //写入
         Mat imgResized;
-        resize(img, imgResized, size);
-        for(int j=0; j<30; j++){
+        resize(img, imgResized, finalSize);
+        for(int j=0; j<60; j++){// 60 frames = 2 seconds
             writer.write(imgResized);
         }
-//        writer.write(img);
     }
+
+    //handle video
+    VideoCapture capture = VideoCapture("1.avi");
+    Mat frame,frameResized;
+    if( !capture.isOpened() ){
+        throw "Error when reading avi";
+    }
+    namedWindow( "w", 1);
+    while(true) {
+        capture >> frame;
+        if(frame.empty())
+            break;
+        resize(frame, frameResized, finalSize);
+        putText(frameResized, "3150104785 HaoGuangBo", cvPoint(30,320), FONT_HERSHEY_COMPLEX_SMALL, 1.0, cvScalar(200,200,250), 1, CV_AA);
+        writer.write(frameResized);
+    }
+
     writer.release();
 
     return 0;
