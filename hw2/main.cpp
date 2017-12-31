cdc34082fa097df83a074a0d9304a72194732063
awmleer <awmleer@sparker.xyz>
2017-12-31 23:44:58 +0800

    ix and iy

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -5,17 +5,67 @@
 using namespace cv;
 using namespace std;
 
+char* imagePath;
+int apertureSize;
+double k;
+
+void calculateIxAndIy(Mat imgGray, Mat &ix, Mat &iy);
+
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
 
     if (argc != 4) {
         return 0;
     }
 
-    char* path = argv[1];
-    double k = atof(argv[2]);
-    int apertureSize = atoi(argv[3]);
+    imagePath = argv[1];
+    k = atof(argv[2]);
+    apertureSize = atoi(argv[3]);
 
+    Mat imgGray;
+    imgGray = imread(imagePath, CV_LOAD_IMAGE_GRAYSCALE);
+
+    namedWindow("grayscale", CV_WINDOW_AUTOSIZE);
+    imshow("grayscale", imgGray);
+    imwrite("grayscale.jpg",imgGray);
+
+    Mat ix=imgGray.clone();
+    Mat iy=imgGray.clone();
+    calculateIxAndIy(imgGray, ix, iy);
+    namedWindow("ix", CV_WINDOW_AUTOSIZE);
+    namedWindow("iy", CV_WINDOW_AUTOSIZE);
+    imshow("ix", ix);
+    imshow("iy", iy);
+
+//    Scalar t = imgGray.at<uchar>(Point(20,20));
+//    printf("%f", t.val[0]);
+
+    waitKey(0);
 
     return 0;
 }
+
+
+void calculateIxAndIy(const Mat imgGray, Mat &ix, Mat &iy){
+    for(int a=0; a<imgGray.rows; a++){
+        for(int b=0; b<imgGray.cols; b++){
+            if(a>=apertureSize&&a<imgGray.rows-apertureSize&&b>apertureSize&&b<imgGray.cols-apertureSize){
+                int tx=0;
+                int ty=0;
+                for(int i=-apertureSize; i<=apertureSize; i++){
+                    tx+=(int)imgGray.at<uchar>(a+apertureSize, b+i);
+                    tx-=(int)imgGray.at<uchar>(a-apertureSize, b+i);
+                    ty+=(int)imgGray.at<uchar>(a+i, b+apertureSize);
+                    ty-=(int)imgGray.at<uchar>(a+i, b-apertureSize);
+                }
+                ix.at<uchar>(a,b)=(uchar)abs(tx/(apertureSize*2+1));
+                iy.at<uchar>(a,b)=(uchar)abs(ty/(apertureSize*2+1));
+            }else{
+                ix.at<uchar>(a,b)=0;
+                iy.at<uchar>(a,b)=0;
+            }
+        }
+    }
+}
+
+
+
