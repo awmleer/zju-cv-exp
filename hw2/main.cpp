102d9eedf9481f2d23379a6cac04b355f8f833c8
awmleer <awmleer@sparker.xyz>
2018-01-01 00:43:18 +0800

    calculate eigen max and min

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -12,6 +12,7 @@ double k;
 Mat imgGray;
 Mat ix,iy;
 Mat eigenMax, eigenMin;
+Mat eigenMaxInt, eigenMinInt;
 
 void calculateIxAndIy();
 void calculateEigenMaxAndEigenMin();
@@ -44,7 +45,15 @@ int main(int argc, char *argv[]) {
 
     eigenMax=imgGray.clone();
     eigenMin=imgGray.clone();
+    eigenMaxInt = Mat(imgGray.rows, imgGray.cols, CV_64FC1);
+    eigenMinInt = Mat(imgGray.rows, imgGray.cols, CV_64FC1);
     calculateEigenMaxAndEigenMin();
+    namedWindow("eigenMax", CV_WINDOW_AUTOSIZE);
+    namedWindow("eigenMin", CV_WINDOW_AUTOSIZE);
+    imshow("eigenMax", ix);
+    imshow("eigenMin", iy);
+    imwrite("eigenMax.jpg", ix);
+    imwrite("eigenMin.jpg", iy);
 
     waitKey(0);
 
@@ -53,22 +62,22 @@ int main(int argc, char *argv[]) {
 
 
 void calculateIxAndIy(){
-    for(int a=0; a<imgGray.rows; a++){
-        for(int b=0; b<imgGray.cols; b++){
-            if(a>=apertureSize&&a<imgGray.rows-apertureSize&&b>apertureSize&&b<imgGray.cols-apertureSize){
+    for(int x=0; x<imgGray.rows; x++){
+        for(int y=0; y<imgGray.cols; y++){
+            if(x>=apertureSize&&x<imgGray.rows-apertureSize&&y>apertureSize&&y<imgGray.cols-apertureSize){
                 int tx=0;
                 int ty=0;
                 for(int i=-apertureSize; i<=apertureSize; i++){
-                    tx+=(int)imgGray.at<uchar>(a+apertureSize, b+i);
-                    tx-=(int)imgGray.at<uchar>(a-apertureSize, b+i);
-                    ty+=(int)imgGray.at<uchar>(a+i, b+apertureSize);
-                    ty-=(int)imgGray.at<uchar>(a+i, b-apertureSize);
+                    tx+=(int)imgGray.at<uchar>(x+apertureSize, y+i);
+                    tx-=(int)imgGray.at<uchar>(x-apertureSize, y+i);
+                    ty+=(int)imgGray.at<uchar>(x+i, y+apertureSize);
+                    ty-=(int)imgGray.at<uchar>(x+i, y-apertureSize);
                 }
-                ix.at<uchar>(a,b)=(uchar)abs(tx/(apertureSize*2+1));
-                iy.at<uchar>(a,b)=(uchar)abs(ty/(apertureSize*2+1));
+                ix.at<uchar>(x,y)=(uchar)abs(tx/(apertureSize*2+1));
+                iy.at<uchar>(x,y)=(uchar)abs(ty/(apertureSize*2+1));
             }else{
-                ix.at<uchar>(a,b)=0;
-                iy.at<uchar>(a,b)=0;
+                ix.at<uchar>(x,y)=0;
+                iy.at<uchar>(x,y)=0;
             }
         }
     }
@@ -76,7 +85,29 @@ void calculateIxAndIy(){
 
 
 void calculateEigenMaxAndEigenMin(){
-
+    for(int x=0; x<imgGray.rows; x++){
+        for(int y=0; y<imgGray.cols; y++){
+            if(x>=apertureSize&&x<imgGray.rows-apertureSize&&y>apertureSize&&y<imgGray.cols-apertureSize){
+                Mat m = Mat(2, 2, CV_64FC1);
+                int ixVal=ix.at<uchar>(x,y)*ix.at<uchar>(x,y);
+                int iyVal=iy.at<uchar>(x,y)*iy.at<uchar>(x,y);
+                m.at<int>(0,0)=ixVal*ixVal;
+                m.at<int>(1,0)=ixVal*iyVal;
+                m.at<int>(0,1)=ixVal*iyVal;
+                m.at<int>(1,1)=iyVal*iyVal;
+                Mat evects = Mat(2, 2, CV_64FC1);
+                Mat evalues = Mat(1, 2, CV_64FC1);
+                eigen(m,evalues,evects);
+                eigenMax.at<uchar>(x,y)=(uchar)(evalues.at<int>(0,0)/65536);
+                eigenMaxInt.at<int>(x,y)=evalues.at<int>(0,0);
+                eigenMin.at<uchar>(x,y)=(uchar)(evalues.at<int>(0,1)/65536);
+                eigenMinInt.at<int>(x,y)=evalues.at<int>(0,1);
+            }else{
+                eigenMax.at<uchar>(x,y)=0;
+                eigenMin.at<uchar>(x,y)=0;
+            }
+        }
+    }
 }
 
 
