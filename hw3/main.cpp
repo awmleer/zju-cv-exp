7a5ee67ddef0e8c286ed65387686203101c8f222
awmleer <awmleer@sparker.xyz>
2018-01-04 15:26:33 +0800

    add threshold and finish hw3

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -7,22 +7,30 @@ using namespace std;
 int main(int argc, char *argv[]) {
     if(argc==1) return 0;
     char* imgPath = argv[1];
-    Mat img = imread(imgPath, 0);
-    imshow("origin", img);
+    double thresh = 128;
+    if(argc>=3){
+        thresh = atof(argv[2]);
+    }
+    Mat originImg = imread(imgPath, CV_LOAD_IMAGE_COLOR);
+    Mat grayscaleImg = imread(imgPath, CV_LOAD_IMAGE_GRAYSCALE);
+    imshow("origin", originImg);
+    Mat binaryImg;
+    threshold(grayscaleImg, binaryImg, thresh, 255, CV_THRESH_BINARY);
+    imshow("binary", binaryImg);
     vector<vector<Point>> contours;
-    findContours(img, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+    findContours(binaryImg, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
     cout<<contours.size()<<endl;
     for(int i=0; i<contours.size(); i++){
         if(contours[i].size()<6)continue;
         Mat points;
         Mat(contours[i]).convertTo(points, CV_32FC3);
         RotatedRect box = fitEllipse(points);
-        ellipse(img,box,Scalar(94, 206, 165), 2, LINE_AA);
+        ellipse(originImg,box,Scalar(94, 206, 165), 2, LINE_AA);
         cout<<box.center.x<<endl;
         cout<<box.center.y<<endl;
         cout<<"----"<<endl;
     }
-    imshow("result",img);
+    imshow("result",originImg);
     waitKey(0);
     return 0;
 }
