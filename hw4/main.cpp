fcb9cdd044462f823ff5b729aafc1e96d218b6ff
awmleer <awmleer@sparker.xyz>
2018-01-14 20:11:45 +0800

    init hw4

---
+++ hw4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv/highgui.h>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
