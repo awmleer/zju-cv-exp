8810c676283d9ae4b4317a4da476a4f6b8ec7c5c
awmleer <awmleer@sparker.xyz>
2018-01-15 17:07:07 +0800

    calc eigen faces

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -10,7 +10,7 @@ class Person;
 
 class Face {
 public:
-    Mat originImg;
+    Mat img;
     Person* person;
     explicit Face(Person* p):person(p){}
 };
@@ -28,7 +28,10 @@ vector<Mat> allFaceMats;
 
 Size standardSize = Size(25,25);
 Mat covarMat = Mat();
-Mat meanMat = Mat(standardSize.height, standardSize.width, CV_64F);
+Mat meanMat = Mat();
+Mat eigenVectors = Mat();
+Mat eigenValues = Mat();
+vector<Mat> eigenFaces;
 
 void readFaces();
 void calcEigens();
@@ -49,8 +52,9 @@ void readFaces(){
         for(int j=1; j<=10; j++){
             Face face = Face(&person);
             Mat img = imread("face-library/"+to_string(i)+"/s"+to_string(j)+".bmp", CV_LOAD_IMAGE_GRAYSCALE);
-            resize(img, face.originImg, standardSize);
-            allFaceMats.push_back(face.originImg);
+            resize(img, face.img, standardSize);
+            normalize(face.img, face.img, 255, 0, NORM_MINMAX);
+            allFaceMats.push_back(face.img);
             person.faces.push_back(&face);
             allFaces.push_back(&face);
         }
@@ -59,9 +63,26 @@ void readFaces(){
 
 void calcEigens(){
     calcCovarMatrix(allFaceMats,covarMat,meanMat,COVAR_NORMAL);
-    imshow("covariance",covarMat);
+//    imshow("covariance",covarMat);
     Mat meanImg;
     meanMat.convertTo(meanImg, CV_8UC1);
     imshow("mean",meanImg);
-
+    eigen(covarMat,eigenValues,eigenVectors);
+    cout<<eigenVectors.rows<<" "<<eigenVectors.cols<<endl;
+    Mat t = Mat(standardSize.height, standardSize.width, CV_64F);
+    Mat tt = Mat(standardSize.height, standardSize.width, CV_8UC1);
+//    eigenValues.row(4).reshape(0,25).convertTo(t, CV_8UC1);
+//    cout<<eigenVectors.type()<<endl;
+    for(int i=0; i<10; i++){
+        for(int j=0; j<standardSize.width*standardSize.height; j++){
+            t.at<double>(j/standardSize.width,j%standardSize.width)=eigenVectors.at<double>(i,j);
+//        cout<<j<<" "<<eigenVectors.at<double>(1,j)<<endl;
+        }
+        normalize(t,t,255,0,NORM_MINMAX);
+        t.convertTo(tt,CV_8UC1);
+        eigenFaces.push_back(tt);
+    }
+    for(int i=0; i<10; i++){
+        imshow("eigen face "+to_string(i), eigenFaces.at(i));
+    }
 }
